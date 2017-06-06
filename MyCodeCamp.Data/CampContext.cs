505d69b0e8a283851a2a3c3006e5c08ee900bed7
pantony89 <pantony89@gmail.com>
2017-06-06 14:10:12 -0400

    Entities Dbcontext

--- MyCodeCamp.Data/CampContext.cs
+++ MyCodeCamp.Data/CampContext.cs
@@ -24,32 +24,5 @@ namespace MyCodeCamp.Data
     public DbSet<Speaker> Speakers { get; set; }
     public DbSet<Talk> Talks { get; set; }
 
-    protected override void OnModelCreating(ModelBuilder builder)
-    {
-      base.OnModelCreating(builder);
-
-      builder.Entity<Camp>()
-        .Property(c => c.Moniker)
-        .IsRequired();
-      builder.Entity<Camp>()
-        .Property(c => c.RowVersion)
-        .ValueGeneratedOnAddOrUpdate()
-        .IsConcurrencyToken();
-      builder.Entity<Speaker>()
-        .Property(c => c.RowVersion)
-        .ValueGeneratedOnAddOrUpdate()
-        .IsConcurrencyToken();
-      builder.Entity<Talk>()
-        .Property(c => c.RowVersion)
-        .ValueGeneratedOnAddOrUpdate()
-        .IsConcurrencyToken();
-    }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-      base.OnConfiguring(optionsBuilder);
-
-      optionsBuilder.UseSqlServer(_config["Data:ConnectionString"]);
-    }
   }
 }
---
+++ MyCodeCamp.Data/Entities/Initial.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCodeCamp.Data.Entities
+{
+    public class Initial
+    {
+        public int KEY { get; set; }
+        public string  InitialCodent { get; set; }
+    }
+}
