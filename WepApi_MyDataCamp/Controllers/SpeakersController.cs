5addf2e386613f5fbe59c78144cd27983551713b
pantony89 <pantony89@gmail.com>
2017-06-06 14:34:12 -0400

    SpeakerController

---
+++ WepApi_MyDataCamp/Controllers/SpeakersController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WepApi_MyDataCamp.Controllers
+{
+    public class SpeakersController:Controller
+    {
+      
+
+        [HttpGet]
+        public IActionResult Get(string moniker, bool includeTalks = false)
+        {
+
+            var speakers = includeTalks ? _repository.GetSpeakersByMonikerWithTalks(moniker) : _repository.GetSpeakersByMoniker(moniker);
+
+            return Ok(_mapper.Map<IEnumerable<SpeakerModel>>(speakers));
+        }
+    }
+}
